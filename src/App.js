ab2ab3de4186ecaf36655a18df0af7350a24ae6c
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-05-05 20:04:19 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="parent">
 
      <div className="Title">
-       FormLab
+       Form  <span style={{color:"#2EAB7D"}}>L</span>ab
        </div>
        <div className="home">
          Home
@@ -15,7 +15,24 @@ export default function App() {
        Lab Test
        </div>
        <div className="next">
-         
+         <div className="inside">
+         Get hassle-free lab tests from the 
+comfort of your home
+         </div>
+         <div className="content">
+         Choose from the list of packages, We care for your health!
+           </div>
+           <div className="search">
+             <div className="Search_Contant">
+             Search for test, packages…
+             </div>
+            
+               
+             <button className='btn'>nithish</button>
+            
+            
+           
+           </div>
        </div>
     </div>
   );
